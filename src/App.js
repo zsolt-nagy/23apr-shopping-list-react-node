0243123f282163dcc9218c293f2aaa0f08bcf3f2
Zsolt Nagy <info@zsoltnagy.eu>
2023-08-01 01:59:22 +0200

    load data from server

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
+
 import ShoppingForm from "./Components/ShoppingForm/ShoppingForm";
 import ShoppingList from "./Components/ShoppingList/ShoppingList";
 
+import "./App.css";
+
 function App() {
+    const [shoppingList, setShoppingList] = useState([]);
+
+    useEffect(() => {
+        fetch("https://3vk7hm-8080.csb.app/api/items")
+            .then((x) => x.json())
+            .then((response) => {
+                setShoppingList(response);
+            });
+    }, []);
+
     return (
         <div className="App">
             <header className="App-header">
@@ -11,6 +24,7 @@ function App() {
             <main>
                 <ShoppingForm />
                 <ShoppingList />
+                {JSON.stringify(shoppingList)}
             </main>
         </div>
     );
