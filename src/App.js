2eb1ba0a19174c314a03c47c7cbc30024b89d7e4
Zsolt Nagy <info@zsoltnagy.eu>
2023-08-01 01:53:20 +0200

    boilerplate with form and list components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ShoppingForm from "./Components/ShoppingForm/ShoppingForm";
+import ShoppingList from "./Components/ShoppingList/ShoppingList";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Shopping List</h1>
+            </header>
+            <main>
+                <ShoppingForm />
+                <ShoppingList />
+            </main>
+        </div>
+    );
 }
 
 export default App;
